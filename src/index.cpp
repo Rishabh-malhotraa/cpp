50b6bf98f1e31ef34d33724d713e5a1ce2b2ce18
Rishabh Malhotra <Rishabhmalhotraa01@gmail.com>
2020-12-02 22:36:10 +0530

    updated

--- src/index.cpp
+++ src/index.cpp
@@ -11,71 +11,78 @@ using namespace std;
 #define deb(x) cout << #x << " " << x << endl;
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MEMSET(x) memset(x, 0, sizeof x)
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 #define FO(i, n) for (int i = 0; i < (n); i++)
 #define FOR(i, n) for (int i = (n); i >= 0; i--)
 #define FOA(i, a, b) for (int i = (a); i < (b); i++)
 
-int LCS(string s1, string s2, int m, int n)
+int minJumpsUtil(vector<int> &arr, int pos, vector<int> memo, vector<bool> &visited)
 {
-    if (m == 0 || n == 0)
-        return 0;
+	if (pos == arr.size() - 1)
+		return 0;
+	if (pos >= arr.size() || pos < 0)
+		return 0;
+	if (visited[pos] == true)
+		return 0;
 
-    else if (s1[m] == s2[n])
-        return 1 + LCS(s1, s2, m - 1, n - 1);
+	visited[pos] = true;
 
-    return max(LCS(s1, s2, m - 1, n), LCS(s1, s2, m, n - 1));
+	int el = arr[pos];
+
+	vector<int>::iterator it = find(arr.begin() + pos, arr.end(), el);
+
+	if (memo[pos])
+		return memo[pos];
+
+	if (it != arr.end())
+	{
+		memo[pos] = 1 + min(min(minJumpsUtil(arr, pos - 1, memo, visited), minJumpsUtil(arr, pos + 1, memo, visited)), minJumpsUtil(arr, distance(arr.begin(), it), memo, visited));
+	}
+	else
+	{
+		memo[pos] = 1 + min(minJumpsUtil(arr, pos - 1, memo, visited), minJumpsUtil(arr, pos + 1, memo, visited));
+	}
+
+	return memo[pos];
 }
 
-int LCSDP(string s1, string s2, int m, int n)
+int minJumps(vector<int> &arr)
 {
-    int dp[m + 1][n + 1];
-    memset(dp, 0, sizeof(dp));
-
-    for (int i = 1; i < m + 1; i++)
-    {
-        FOA(j, 1, n + 1)
-        {
-
-            if (s1[i - 1] == s2[j - 1])
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            else
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-        }
-    }
-
-    return dp[m][n];
+	// cam we use the sweep technique?
+	// 100--23--23--404--100--23--23--23--3--404
+	int n = arr.size();
+	vector<bool> visited(n, false);
+	vector<int> memo(n, 0);
+	return minJumpsUtil(arr, 0, memo, visited);
 }
 
 void solve()
 {
-    string s1, s2;
-    cin >> s1 >> s2;
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
-    // using recurssion
-    int val = LCS(s1, s2, s1.length(), s2.length());
-    // using dp
-    int valDP = LCSDP(s1, s2, s1.length(), s2.length());
-
-    cout << "RECURSSION solution " << val << endl;
-    cout << "DYNAMIC PROGRAMMING solution " << valDP << endl;
-    return;
+	cout << minJumps(arr) << endl;
+	return;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 #endif
 
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        solve();
-    }
-    return 0;
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
 }
